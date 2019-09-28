309f4b17a3ed3fbe0177095a774e7169bc5346a1
fancyaction <labcoatdisaster@gmail.com>
2019-09-28 15:08:08 -0500

    Add and test validateInput

--- src/Calculation.js
+++ src/Calculation.js
@@ -6,13 +6,20 @@ export default class Calculation {
 
     getNumber = value => Number.isNaN(parseInt(value)) ? 0 : parseInt(value);
 
+    validateInput = inputs => {
+        const invalidNumbers = inputs.filter(val => -1 === Math.sign(val));
+
+        if (0 < invalidNumbers.length) {
+            throw Error(`These negative inputs are not allowed: ${invalidNumbers}`);
+        }
+    }
+
     getTotal = () => {
         const { inputValue, delimiter } = this;
-
         const inputsArr = inputValue.split(delimiter);
 
+        this.validateInput(inputsArr);
         return inputsArr.reduce((prev, curr) => prev + this.getNumber(curr), 0);
-
     }
 
 }
--- src/Calculation.spec.js
+++ src/Calculation.spec.js
@@ -33,4 +33,23 @@ describe('Calculation supports a maximum of 2 numbers using a comma delimiter', 
         expect(calculationTotal).toBe(5);
     })
 
+    it('Combines unlimited number of inputs', () => {
+        const calculationTotal = new Calculation(delimiter, '1,2,3,4,5,6,7,8,9,10,11,12').getTotal();
+
+        expect(calculationTotal).toBe(78);
+    })
+    
+    it('Supports newline character as an alternative delimiter', () => {
+        const calculationTotal = new Calculation(delimiter, '1\n2,3').getTotal();
+
+        expect(calculationTotal).toBe(6);
+    })
+
+    it('Returns error containing array of negative numbers', () => {
+        try {
+            new Calculation(delimiter, '-3,-6,-11').getTotal();
+        } catch (e) {
+            expect(e.message).toBe('These negative inputs are not allowed: -3,-6,-11');
+        }
+    })
 })
