1fd13c2eaa1d444c5b515c588119f2ab7b7b2293
fancyaction <labcoatdisaster@gmail.com>
2019-09-28 11:37:14 -0500

    Remove depracted code from test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-
+import { shallow } from 'enzyme';
 import App from './App';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const findByTestAttr = (wrapper, val) => {
     return wrapper.find(`[data-test="${val}"]`);
