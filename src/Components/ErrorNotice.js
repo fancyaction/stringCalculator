c8ee40979ec63bae8f9b6369172cb28208d9c668
fancyaction <labcoatdisaster@gmail.com>
2019-09-29 14:54:50 -0500

    Setup ErrorNotice

---
+++ src/Components/ErrorNotice.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import ErrorIcon from '@material-ui/icons/Error';
+
+const useStyles = makeStyles(theme => ({
+    close: {
+        padding: theme.spacing(0.5),
+    },
+}));
+
+const ErrorNotice = ({ text, show, onClose }) => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            <Snackbar
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                }}
+                open={show}
+                autoHideDuration={2500}
+                onClose={onClose}
+                ContentProps={{
+                    'aria-describedby': 'message-id',
+                }}
+                message={<span id="message-id">{text}</span>}
+                action={[
+                    <IconButton
+                        key="close"
+                        aria-label="close"
+                        color="inherit"
+                        className={classes.close}
+                        onClick={onClose}
+                    >
+                        <ErrorIcon />
+                    </IconButton>,
+                ]}
+            />
+        </div>
+    );
+}
+
+export default ErrorNotice;
